18d7b54d2f66a391b585f648bc5c396b0c21337f
Braian Velarde <braianvelarde@gmail.com>
2022-05-12 17:11:14 -0300

    middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,7 @@
 import type { NextFetchEvent, NextRequest } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const ip = req.ip;
+  console.log(ip);
   return new Response("hello");
 }

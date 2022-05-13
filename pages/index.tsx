45d982a6c2d25d86c5ac09d5b36716c25ee0dcef
Braian Velarde <braianvelarde@gmail.com>
2022-05-13 02:50:18 -0300

    whois working with useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,54 @@
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import useSWR from "swr";
 import { who } from "../types";
+import axios from "axios";
 
-const Home: NextPage = ({ ip }: any) => {
-  const fetcher = (...args: [string]) =>
-    fetch(...args, { method: "GET" }).then((res) => res.json());
-  const { data, error } = useSWR<who>(
-    `https://rdap.lacnic.net/rdap/ip/${ip}`,
-    fetcher
-  );
+const Home: NextPage = () => {
+  const [ip, setIp] = useState();
+  const [ipData, setIpData] = useState<any>();
+  // const fetcher = (...args: [string]) =>
+  //   fetch(...args, { method: "GET" }).then((res) => res.json());
+  // const { data, error } = useSWR<who>(
+  //   `https://rdap.lacnic.net/rdap/ip/${ip}`,
+  //   fetcher
+  // );
 
-  if (error) return <div>failed to load</div>;
-  if (!data) return <div>loading...</div>;
+  useEffect(() => {
+    const fetchIp = axios("https://api.ipify.org?format=json")
+      .then((res) => {
+        setIp(res.data.ip);
+        axios(`https://rdap.lacnic.net/rdap/ip/${res.data.ip}`)
+          .then((datares) => setIpData(datares.data))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => err);
+  }, []);
+
+  // if (error) return <div>failed to load</div>;
+  if (!ipData) return <div>loading...</div>;
   // renderizar datos
-
   return (
     <div>
-      hello {ip}
-      <p>rango de ip {data.startAddress + "/" + data.endAddress}</p>
-      <p>Representante legal: {data.lacnic_legalRepresentative || "no info"}</p>
+      <p>Hola! Tu ip es: {ip}</p>
       <p>
-        Registrante: {data.entities[0].handle} from{" "}
-        {data.entities[0].vcardArray.map((item) => (
+        Tu proveedor es:{" "}
+        {ipData.entities[0].vcardArray.map((item) => (
           <span key={item[1][1]}>{item[1][3]}</span>
-        ))}
+        ))}{" "}
+        <span key={ipData.entities[0].handle}>
+          ({ipData.entities[0].handle})
+        </span>
+      </p>
+      <p>
+        El rangorango de ip del proveedor es{" "}
+        {ipData.startAddress + "/" + ipData.endAddress}
+      </p>
+      <p>
+        Representante legal: {ipData.lacnic_legalRepresentative || "no info"}
       </p>
     </div>
   );
 };
-
-export async function getServerSideProps() {
-  const data = await (await fetch("https://api.ipify.org?format=json")).json();
-
-  return {
-    props: {
-      ip: data.ip,
-    },
-  };
-}
 
 export default Home;
